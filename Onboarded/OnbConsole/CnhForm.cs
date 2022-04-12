abe1a44826bdcb447aa2b97a5620ae00e3de2c68
dansemedo <daniel.semedo@outlook.com>
2022-04-12 18:41:28 -0300

    Create console test app structure, methods and classes

---
+++ Onboarded/OnbConsole/CnhForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnbConsole
+{
+    public class CnhForm
+    {
+      public string IdCnh;
+      public string Name;
+      public string Cpf;
+    }
+}
---
+++ Onboarded/OnbConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OnbConsole;
+
+Console.WriteLine("Onboarded App");
+
+var x = ExtractForm();
+var validCnh = ValidateCnh(x);
+
+if (validCnh == true)
+{
+    SaveCnhDatabase();
+    SaveCnhStorage();
+    Console.WriteLine("CNH validada e processo de onboard realizado com sucesso!");
+}
+else if (validCnh == false)
+{
+    Console.WriteLine("CNH não é valida, repita o processo");
+}
+
+
+CnhForm ExtractForm()
+{
+    var cnhForm = new CnhForm
+    {
+        Name  = "Daniel",
+        IdCnh = "12223424",
+        Cpf   = "23422123-8"       
+    };
+
+    return cnhForm;
+}
+
+Boolean ValidateCnh(CnhForm x)
+{
+    if (x.Cpf == "23422123-7")
+    return true;
+    else
+    return false;
+}
+
+void SaveCnhStorage()
+{
+    Console.WriteLine("salvo no cosmos");
+}
+
+void SaveCnhDatabase()
+{
+    Console.WriteLine("salvo no storage");
+}
+
